f4e09bc98fcacddd631952bb1e3b28d9f603f149
fl1k <vgolubovic97@gmail.com>
2024-11-23 17:05:34 +0100

    fix

--- main.py
+++ main.py
@@ -11,14 +11,16 @@ GPIO.setup(PIN, GPIO.OUT)  # Set GPIO 17 as an output
 try:
     while True:
         # Turn on the GPIO pin
+        time.sleep(10)  # Keep it off for 1 second
+        
         GPIO.output(PIN, GPIO.HIGH)
         print("GPIO 17 ON")
-        time.sleep(1)  # Keep it on for 1 second
+        time.sleep(5)  # Keep it on for 1 second
 
         # Turn off the GPIO pin
         GPIO.output(PIN, GPIO.LOW)
         print("GPIO 17 OFF")
-        time.sleep(30)  # Keep it off for 1 second
+        time.sleep(20)  # Keep it off for 1 second
 
 except KeyboardInterrupt:
     print("Script interrupted by user.")
