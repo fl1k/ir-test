51fc831acf14ac39a2037d06e6e81a45ac7da5ef
fl1k <vgolubovic97@gmail.com>
2024-11-23 15:15:13 +0100

    change pin

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import RPi.GPIO as GPIO
 import time
 
 # Pin configuration
-PIN = 16
+PIN = 11
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)  # Use Broadcom pin numbering
-GPIO.setup(PIN, GPIO.OUT)  # Set GPIO 23 as an output
+GPIO.setup(PIN, GPIO.OUT)  # Set GPIO 17 as an output
 
 try:
     while True:
         # Turn on the GPIO pin
         GPIO.output(PIN, GPIO.HIGH)
-        print("GPIO 26 ON")
+        print("GPIO 17 ON")
         time.sleep(1)  # Keep it on for 1 second
 
         # Turn off the GPIO pin
         GPIO.output(PIN, GPIO.LOW)
-        print("GPIO 26 OFF")
+        print("GPIO 17 OFF")
         time.sleep(1)  # Keep it off for 1 second
 
 except KeyboardInterrupt:
